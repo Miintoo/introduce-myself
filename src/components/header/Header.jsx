a85e0f4b0be72f04ab89eadd153e335ee72c295c
Minto <xpeed96@gmail.com>
2023-07-17 23:45:37 +0900

    Add: Header 컴포넌트, Header scss 파일

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './Header.scss';
+
+export default function Header() {
+  return (
+    <header>
+      <h1>MINTO</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Skill</li>
+        <li>Contact</li>
+      </ul>
+    </header>
+  );
+}
