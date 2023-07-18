3189e9d32eb13672471e4bcc5632888ebdd4da22
Minto <xpeed96@gmail.com>
2023-07-18 11:38:39 +0900

    Add: Header 컴포넌트 HTML 및 CSS 작업

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,9 +3,9 @@ import './Header.scss';
 
 export default function Header() {
   return (
-    <header>
-      <h1>MINTO</h1>
-      <ul>
+    <header className="header">
+      <h1 className="title">MINTO</h1>
+      <ul className="nav_container">
         <li>Home</li>
         <li>About</li>
         <li>Skill</li>
