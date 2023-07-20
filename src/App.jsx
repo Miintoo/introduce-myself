a20fac8846d3018bccec752d890fb0f1434e792e
Minto <xpeed96@gmail.com>
2023-07-20 20:13:54 +0900

    Add: App 컴포넌트 Footer 추가

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Home from './components/home/Home';
 import About from './components/about/About';
 import Skill from './components/skill/Skill';
 import Contact from './components/contact/Contact';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <About />
       <Skill />
       <Contact />
+      <Footer />
     </>
   );
 }
