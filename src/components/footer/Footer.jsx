64a95bd2bd3ad4f9c8b5350da3ff43ae8247c091
Minto <xpeed96@gmail.com>
2023-07-20 20:14:57 +0900

    Add: Footer HTML 작성 및 CSS 작성

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Footer.scss';
+
+export default function Footer() {
+  return (
+    <div className="footer_container">
+      <p>개인 포트폴리오를 소개하는 페이지입니다.</p>
+    </div>
+  );
+}
