4a3e23f6d767186e8e6c2195cac46ac0e77bdb7b
Minto <xpeed96@gmail.com>
2023-07-22 07:59:55 +0900

    Style: contact 컴포넌트 수직 중앙 정렬 속성 추가

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -4,24 +4,26 @@ import styles from './Contact.module.scss';
 export default forwardRef(function Contact(props, ref) {
   return (
     <div className={styles.container} ref={ref}>
-      <h2 className={styles.title}>Contact Me</h2>
-      <ul className={styles.contact_logo_container}>
-        <li>
-          <a href="https://github.com/Miintoo">
-            <img src="/assets/github.png" alt="깃헙 로고 이미지" />
-          </a>
-        </li>
-        <li>
-          <a href="https://velog.io/@xpeed96">
-            <img src="/assets/velog.png" alt="블로그 로고 이미지" />
-          </a>
-        </li>
-        <li>
-          <a href="mailto:xpeed96@gmail.com">
-            <img src="/assets/mail.png" alt="메일 로고 이미지" />
-          </a>
-        </li>
-      </ul>
+      <div>
+        <h2 className={styles.title}>Contact Me</h2>
+        <ul className={styles.contact_logo_container}>
+          <li>
+            <a href="https://github.com/Miintoo">
+              <img src="/assets/github.png" alt="깃헙 로고 이미지" />
+            </a>
+          </li>
+          <li>
+            <a href="https://velog.io/@xpeed96">
+              <img src="/assets/velog.png" alt="블로그 로고 이미지" />
+            </a>
+          </li>
+          <li>
+            <a href="mailto:xpeed96@gmail.com">
+              <img src="/assets/mail.png" alt="메일 로고 이미지" />
+            </a>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 });
