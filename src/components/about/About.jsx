0b55ba5fc4111d3b1074468af44311c606897816
Minto <xpeed96@gmail.com>
2023-07-22 22:30:00 +0900

    Feat: About 컴포넌트 HTML 수정

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,25 +1,20 @@
 import React, { forwardRef } from 'react';
 import styles from './About.module.scss';
+import Profile from '../../assets/profile.png';
 
 export default forwardRef(function About(props, ref) {
   return (
     <div className={styles.container} ref={ref}>
       <h2 className={styles.title}>About Me</h2>
-      <div className={styles.container_item}>
-        <img src="/assets/elice.png" alt="교육 과정 로고 입니다." />
-        <div>
-          <div>
-            <h3>엘리스 IOT 1기</h3>
-            <span>2022.12 ~ 2023.05 수료</span>
-          </div>
-        </div>
-      </div>
-      <div className={styles.container_item}>
-        <img src="/assets/dankook.webp" alt="학교 로고 입니다." />
-        <div>
-          <h3>단국대학교 (전자전기공학부)</h3>
-          <span>2016.03 ~ 2023.03 졸업</span>
-        </div>
+      <div className={styles.profile}>
+        <img src={Profile} alt="프로필 이미지입니다." />
+        <ul>
+          <li>김민혁</li>
+          <li>1996.04.15</li>
+          <li>단국대학교 전자전기공학부 졸업</li>
+          <li>엘리스 IOT 1기 수료</li>
+          <li>최근에는 Redux, Redux-toolkit, Nextjs에 관심이 생겨 공부하고 있습니다.</li>
+        </ul>
       </div>
     </div>
   );
